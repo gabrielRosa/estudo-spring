1ca1174358b133bb962eed1064eeecedabe64c93
Gabriel F. da Rosa <gabrielfr97@gmail.com>
2018-05-02 22:57:10 -0300

    feat: create domains City.java and State.java

---
+++ Spring/api/src/main/java/com/gabriel/spring/api/domain/City.java
@@ -0,0 +1,66 @@
+package com.gabriel.spring.api.domain;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+public class City implements Serializable {
+
+    private static final long serialVersionUID = 2776842450648945044L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "state_id")
+    private State state;
+
+    public City() { }
+
+    public City(Integer id, String name, State state) {
+        this.id = id;
+        this.name = name;
+        this.state = state;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof City)) return false;
+        City city = (City) o;
+        return Objects.equals(getId(), city.getId());
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(getId());
+    }
+}
---
+++ Spring/api/src/main/java/com/gabriel/spring/api/domain/State.java
@@ -0,0 +1,66 @@
+package com.gabriel.spring.api.domain;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+public class State implements Serializable {
+
+    private static final long serialVersionUID = 1142922571900951559L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @OneToMany(mappedBy = "state")
+    private List<City> cities = new ArrayList<>();
+
+    public State() { }
+
+    public State(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<City> getCities() {
+        return cities;
+    }
+
+    public void setCities(List<City> cities) {
+        this.cities = cities;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof State)) return false;
+        State state = (State) o;
+        return Objects.equals(getId(), state.getId());
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(getId());
+    }
+}
