5b9a4793aa9045607b85f5890d4baf2bc9668ce9
Gabriel F. da Rosa <gabrielfr97@gmail.com>
2018-05-02 22:23:12 -0300

    feat: create StanderError.java

---
+++ Spring/api/src/main/java/com/gabriel/spring/api/Error/StandardError.java
@@ -0,0 +1,45 @@
+package com.gabriel.spring.api.Error;
+
+
+import org.springframework.http.HttpStatus;
+
+import java.io.Serializable;
+
+public class StandardError implements Serializable {
+
+    private static final long serialVersionUID = 8123544297413346209L;
+
+    private HttpStatus httpStatus;
+    private String message;
+    private Long timeStamp;
+
+    public StandardError(HttpStatus httpStatus, String message, Long timeStamp) {
+        this.httpStatus = httpStatus;
+        this.message = message;
+        this.timeStamp = timeStamp;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    public void setHttpStatus(HttpStatus httpStatus) {
+        this.httpStatus = httpStatus;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(Long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+}
