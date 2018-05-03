e39abf695f265ad748459bd6da8bae9845222df9
Gabriel F. da Rosa <gabrielfr97@gmail.com>
2018-05-02 22:24:09 -0300

    feat: create CategoryRepository.java and ProductRepository.java

---
+++ Spring/api/src/main/java/com/gabriel/spring/api/repository/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.gabriel.spring.api.repository;
+
+import com.gabriel.spring.api.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Integer> { }
