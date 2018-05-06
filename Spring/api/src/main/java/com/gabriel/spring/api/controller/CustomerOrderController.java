5dce54be5aca141cc40b27dfd1c7420593c81c98
Gabriel F. da Rosa <gabrielfr97@gmail.com>
2018-05-06 18:55:57 -0300

    feat: create CustomerOrderController

---
+++ Spring/api/src/main/java/com/gabriel/spring/api/controller/CustomerOrderController.java
@@ -0,0 +1,25 @@
+package com.gabriel.spring.api.controller;
+
+import com.gabriel.spring.api.domain.CustomerOrder;
+import com.gabriel.spring.api.service.CustomerOrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/customerOrder")
+public class CustomerOrderController {
+
+	@Autowired
+	private CustomerOrderService customerOrderService;
+	
+	@RequestMapping(value = "/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable final Integer id) {
+		CustomerOrder customerOrder = customerOrderService.find(id);
+
+		return ResponseEntity.ok().body(customerOrder);
+	}
+}
