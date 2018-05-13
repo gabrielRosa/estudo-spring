ba5d3626f4c5c5856e277cb54f52b804f40c0158
Gabriel F. da Rosa <gabrielfr97@gmail.com>
2018-05-13 11:00:24 -0300

    feat: add new methods insert, update and delete in CategoryController

--- Spring/api/src/main/java/com/gabriel/spring/api/controller/CategoryController.java
+++ Spring/api/src/main/java/com/gabriel/spring/api/controller/CategoryController.java
@@ -1,13 +1,16 @@
 package com.gabriel.spring.api.controller;
 
 import com.gabriel.spring.api.domain.Category;
+import com.gabriel.spring.api.dto.CategoryDTO;
 import com.gabriel.spring.api.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value="/category")
@@ -16,10 +19,42 @@ public class CategoryController {
 	@Autowired
 	private CategoryService categoryService;
 	
-	@RequestMapping(value = "/{id}", method=RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable final Integer id) {
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Category> find(@PathVariable final Integer id) {
 		Category category = categoryService.find(id);
 
 		return ResponseEntity.ok().body(category);
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoryDTO>> find() {
+		List<Category> categoryList = categoryService.findAll();
+		List<CategoryDTO> categoryDTOS = categoryList.stream().map(cl -> new CategoryDTO(cl)).collect(Collectors.toList());
+
+		return ResponseEntity.ok().body(categoryDTOS);
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Category category) {
+		category = categoryService.insert(category);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(category.getId()).toUri();
+
+		return ResponseEntity.created(uri).build();
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody Category category, @PathVariable Integer id) {
+		category.setId(id);
+		categoryService.update(category);
+
+		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable final Integer id) {
+		categoryService.delete(id);
+
+		return ResponseEntity.noContent().build();
 	}
 }
