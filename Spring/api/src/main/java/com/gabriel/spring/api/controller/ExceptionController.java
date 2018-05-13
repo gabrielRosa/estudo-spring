4c3e35e27454f6dafaae8eac59409e97ac16172f
Gabriel F. da Rosa <gabrielfr97@gmail.com>
2018-05-13 10:58:31 -0300

    feat: change name and add new method objectDeletedException in NotFoundController

--- Spring/api/src/main/java/com/gabriel/spring/api/controller/ExceptionController.java
+++ Spring/api/src/main/java/com/gabriel/spring/api/controller/ExceptionController.java
@@ -1,6 +1,7 @@
 package com.gabriel.spring.api.controller;
 
-import com.gabriel.spring.api.Error.StandardError;
+import com.gabriel.spring.api.error.StandardError;
+import org.hibernate.ObjectDeletedException;
 import org.hibernate.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,11 +11,19 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import javax.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
-public class NotFoundController {
+public class ExceptionController {
 
     @ExceptionHandler(ObjectNotFoundException.class)
     public ResponseEntity<StandardError> objectNotFoundException(ObjectNotFoundException e, HttpServletRequest request) {
         HttpStatus httpStatus = HttpStatus.NOT_FOUND;
+        StandardError error = new StandardError(httpStatus, e.getMessage(), System.currentTimeMillis());
+
+        return ResponseEntity.status(httpStatus).body(error);
+    }
+
+    @ExceptionHandler(ObjectDeletedException.class)
+    public ResponseEntity<StandardError> objectDeletedException(ObjectDeletedException e, HttpServletRequest request) {
+        HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
         StandardError error = new StandardError(httpStatus, e.getMessage(), System.currentTimeMillis());
 
         return ResponseEntity.status(httpStatus).body(error);
